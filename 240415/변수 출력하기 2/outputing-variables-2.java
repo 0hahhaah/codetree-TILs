ffbd7fcb8285c5dfbc71898f0e3e099a1cee6fdf
Jiho <97162920+0hahhaah@users.noreply.github.com>
2024-04-15 10:08:20 +0900

    Title: 변수 출력하기 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240415/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        char a = 'C';
+        int b = 3;
+
+        System.out.println(a+"!...!"+b);
+    }
+}
