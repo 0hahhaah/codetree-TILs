66444d6f72361e352eae15e1b42b374f3f4ca980
Jiho <97162920+0hahhaah@users.noreply.github.com>
2024-04-15 14:23:37 +0900

    Title: 1시간 뒤 시간 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240415/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 240415/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        sv.useDelimeter(":");
+        sc.useDelimeter(":");
         int a = sc.nextInt();
         int b = sc.nextInt();
 
