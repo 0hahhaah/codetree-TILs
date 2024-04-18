389224f8b324e0c3abe50c8e50231c5412bac1ef
Jiho <97162920+0hahhaah@users.noreply.github.com>
2024-04-18 10:21:59 +0900

    Title: 윤년인가, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240418/윤년인가/is-leap-year.java
+++ 240418/윤년인가/is-leap-year.java
@@ -6,9 +6,15 @@ public class Main {
 
         int y = sc.nextInt();
 
-        if(y%4==0&&y%100==0||y%400!=0) {
-            System.out.print("true");
-        }   else {
+        if(y%4 == 0) {
+            if(y%400==0) {
+                System.out.print("true");
+            } else if(y%100 != 0 ) {
+                System.out.print("true");
+            } else {
+                System.out.print("false");
+            }
+        } else {
             System.out.print("false");
         }
     }
