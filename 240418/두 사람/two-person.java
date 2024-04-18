b2dec80384849cbb50f13de48d9005a6836d9cac
Jiho <97162920+0hahhaah@users.noreply.github.com>
2024-04-18 09:51:49 +0900

    Title: 두 사람, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240418/두 사람/two-person.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a1 = sc.nextInt();
+        char a2 = sc.next().charAt(0);
+        
+
+        int b1 = sc.nextInt();
+        char b2 = sc.next().charAt(0);
+
+        if((a1>=19 && a2 == 'M') || (b1 >= 19 && b2 == 'M')) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
