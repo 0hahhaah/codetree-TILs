d1374c57cae854146b0673dc6ff23eeacd9afc1f
Jiho <97162920+0hahhaah@users.noreply.github.com>
2024-04-18 09:33:04 +0900

    Title: 장학금, Time: 198ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240418/장학금/scholarship.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int mid = sc.nextInt();
+        int end = sc.nextInt();
+
+        if(mid >= 90) {
+            if(end >= 95) {
+                System.out.print(100000);
+            } else if(end >= 90){
+                System.out.print(50000);
+            } else {
+                System.out.print(0);
+            }
+        } else {
+            System.out.print(0);
+        }
+    }
+}
